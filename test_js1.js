ea9df4d87e5f71b076cf1d52dbcbb228b825e9a3
dominik-w <office@directcode.eu>
2014-04-19 22:07:55 +0200

    Test

---
+++ test_js1.js
@@ -0,0 +1,28 @@
+﻿
+/**
+ * Test - JS
+ * 
+ * by javascript-html5-tutorial.com
+ */
+
+function getCarData() {
+
+    var myCar = {
+        name: "VW",
+        model: "Bora",
+        power_hp: 110,
+        getColor: function () {
+            // ...
+        }
+    };
+    
+    alert(myCar.hasOwnProperty('constructor')); // false
+    alert(myCar.hasOwnProperty('getColor')); // true
+    alert(myCar.hasOwnProperty('megaFooBarNonExisting')); // false
+
+    if (myCar.hasOwnProperty('power_hp')) {
+        alert(typeof(myCar.power_hp)); // number
+    }
+}
+
+getCarData();
