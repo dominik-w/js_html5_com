680e29f45dfba39670ab3dae7d37f066d0911c1a
dominik-w <office@directcode.eu>
2014-04-25 11:54:06 +0200

    jquery tutorial - sliding menu

---
+++ jquery_tutorial_sliding_menu/js/menu_funcs.js
@@ -0,0 +1,27 @@
+﻿
+/**
+ * Sliding horizontal menu, using jQuery and jquery.easing plugin
+ * 
+ * by http://javascript-html5-tutorial.com/
+ */
+
+$(document).ready(function () {
+    
+    $("div#jq_menu ul li a").hover(function() {
+        
+        if ($(this).is(":animated")) {
+            $(this).stop().animate({width: "300px"}, {duration: 360, easing: "easeOutQuad"});
+        } else {
+            $(this).stop().animate({width: "300px"}, {duration: 360, easing: "easeOutQuad"});
+        }
+    },
+    turn_back = function () {
+        
+        if ($(this).is(":animated")) {
+            $(this).stop().animate({width: "90px"}, {duration: 360, easing: "easeInOutQuad"});
+        } else {
+            $(this).stop(":animated").animate({width: "90px"}, {duration: 360, easing: "easeInOutQuad"});
+        }
+    });
+    
+});
